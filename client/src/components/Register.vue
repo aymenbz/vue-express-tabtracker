440d2b473780488416456dd904f62ad95b46cae5
Aymen Bouzoumita <aymenweb5@gmail.com>
2018-03-24 20:56:58 +0100

    set up register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>Register</h1>
+    <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="Email" />
+    <br>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="Password" />
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+export default {
+  name: 'register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  watch: {
+
+  },
+  methods: {
+   async register() {
+    const response = await  AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 2000)
+  }
+}
+</script>
